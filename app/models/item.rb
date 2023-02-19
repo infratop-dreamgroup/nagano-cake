75d42ce9cd65493d4a14de603e34267e20a42c9c
EC2 Default User <ec2-user@ip-172-31-16-211.ap-northeast-1.compute.internal>
2023-02-19 17:42:41 +0900

    Add ActiveStorage&ImageProcessing&Bootstrap&kaminari

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   has_many :orders, through: :order_details, dependent: :destroy
   has_many :order_details, dependent: :destroy
   has_many :customers, dependent: :destroy
+  
+  has_one_attached :image
 end

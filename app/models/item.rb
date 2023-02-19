4e586ca1e7a974c9b168c0ec7d143bd540effb3b
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-19 08:07:02 +0000

    Add 各モデルアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  has_many :orders, through: :order_details, dependent: :destroy
+  has_many :order_details, dependent: :destroy
+  has_many :customers, dependent: :destroy
 end

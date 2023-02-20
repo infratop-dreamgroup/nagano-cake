5061fcd5a1f15cd338ce01ae164c63c7d1fcc819
EC2 Default User <ec2-user@ip-172-31-16-211.ap-northeast-1.compute.internal>
2023-02-20 14:35:06 +0900

    Fix route root to

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,21 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+  scope module: :public do
+    root to: "homes#top"
 
-  root to: "homes#top"
-
-  resources :address, only: [:index, :edit, :create, :update, :destroy]
-  resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
-  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
-  resources :customers, only: [:show, :edit, :update, :unsubscribe]
-  resources :items, only: [:index, :show]
-  resources :homes, only: [:top, :about]
-
+    resources :address, only: [:index, :edit, :create, :update, :destroy]
+    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :customers, only: [:show, :edit, :update, :unsubscribe]
+    resources :items, only: [:index, :show]
+    resources :homes, only: [:top, :about]
+  end
 
 
 
   namespace :admin do
+    root to: "homes#top"
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
     resources :customers, only: [:index, :show, :edit, :update]

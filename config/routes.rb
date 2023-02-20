d3eac997a84c824f2b83c7ee2bf729a417465666
toukaeru <senkoutoburakki@outlook.jp>
2023-02-20 13:35:56 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
 
 
   namespace :admin do
+    root to: "homes#top"
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
     resources :customers, only: [:index, :show, :edit, :update]

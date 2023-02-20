10b877075fa69b6a8fc9e51b07595737caad97d3
EC2 Default User <ec2-user@ip-172-31-16-211.ap-northeast-1.compute.internal>
2023-02-20 13:41:46 +0900

    Add ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,71 +1,29 @@
 Rails.application.routes.draw do
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+  resources :address, only: [:index, :edit, :create, :update, :destroy]
+  resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+  resources :customers, only: [:show, :edit, :update, :unsubscribe]
+  resources :items, only: [:index, :show]
+  resources :homes, only: [:top, :about]
+
+
+
+
   namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :homes, only: [:top]
   end
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :admins
-  devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

33e142c8c4bf9acd39f3725c2c41a6499d47957b
toukaeru <senkoutoburakki@outlook.jp>
2023-02-21 13:26:28 +0000

    Add public-orders

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,32 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+
   def new
+    @order = Order.new
+    @addresses = current_customer.addresses.all
   end
 
   def confirm
+    @order = Order.new(order_params)
+
+    if params[:order][:address_option] == "0"
+        @order.shipping_post_code = current_customer.post_code
+        @order.shipping_address = current_customer.address
+        @order.shipping_name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_option] == "1"
+        ship = Address.find(params[:order][:member_id])
+        @order.shipping_post_code = ship.post_code
+        @order.shipping_address = ship.address
+        @order.shipping_name = ship.name
+    elsif params[:order][:address_option] = "2"
+        @order.shipping_post_code = params[:order][:shipping_post_code]
+        @order.shipping_address = params[:order][:shipping_address]
+        @order.shipping_name = params[:order][:shipping_name]
+    else
+         render 'new'
+    end
   end
+end
 
   def complete
   end
@@ -16,4 +39,8 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
-end
+
+  private
+    def order_params
+        params.require(:order).permit(:postage, :payment_method, :shipping_name, :shipping_address, :shipping_post_code ,:member_id,:total_payment,:status)
+    end

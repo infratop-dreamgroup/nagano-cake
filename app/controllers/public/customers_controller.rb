fd3e8041ec91f7a9e615e31d351a48176399ab9d
EC2 Default User <ec2-user@ip-172-31-16-211.ap-northeast-1.compute.internal>
2023-02-22 16:28:27 +0900

    Add バリデーション&退会

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,11 +14,20 @@ class Public::CustomersController < ApplicationController
   end
 
   def unsubscribe
+    @customer = current_customer
+  end
+
+  def withdrawal
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会が完了しました！"
+    redirect_to root_path
   end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :phone_number)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :phone_number, :is_deleted)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,5 +8,15 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :items, through: :cart_items
   has_many :cart_items, dependent: :destroy
-
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/
+  validates :post_code, presence: true, format: { with: VALID_POSTAL_CODE_REGEX }
+  validates :address, presence: true
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }, uniqueness: true
+  validates :is_deleted, inclusion: { in: [true, false] }
 end

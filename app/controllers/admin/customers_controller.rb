111564154c0cac9bc4582d671132a242af1645c6
EC2 Default User <ec2-user@ip-172-31-16-211.ap-northeast-1.compute.internal>
2023-02-22 16:41:24 +0900

    Fix ページ表示数

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customer.page(params[:page]).per(3)
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show

92f7473fbbbddcde6974a644cdef21c02af70590
kiruha <121922228+kiruha81@users.noreply.github.com>
2023-02-24 13:11:12 +0900

    Revert "Nakamoto feature ordersadmin"

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-  scope module: :public do
-    root to: "homes#top"
-    get '/about' => 'homes#about'
-
-    resources :address, only: [:index, :edit, :create, :update, :destroy]
-    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
-    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
-    resources :customers, only: [:show, :edit, :update, :unsubscribe]
-    resources :items, only: [:index, :show]
-    resources :homes, only: [:top, :about]
-  end
-
-
-
-  namespace :admin do
-    root to: "homes#top"
-    resources :orders, only: [:show, :update]
-    resources :order_details, only: [:update]
-    resources :customers, only: [:index, :show, :edit, :update]
-    resources :genres, only: [:index, :create, :edit, :update]
-    resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    resources :homes, only: [:top]
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
